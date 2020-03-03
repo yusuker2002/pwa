faf5267998eff1c2869bc669becc682cf25815a8
jiongyigao <gao.gao@igg.com>
2020-03-03 16:54:00 +0800

    程序调试

--- demo2/public/sw.js
+++ demo2/public/sw.js
@@ -43,7 +43,7 @@ self.addEventListener('activate', function (e) {
       }))
     })
   )
-  return self.clients.claim()
+  //return self.clients.claim()
 })
 
 self.addEventListener('fetch', function (e) {
